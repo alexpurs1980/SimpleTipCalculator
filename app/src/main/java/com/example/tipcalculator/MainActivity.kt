4d407bbfca76e8f2506eb6b65e359444d042c4ec
Aleksey <alexpurs@yandex.ru>
2022-08-28 18:42:51 +0300

    First steps with view group Added string resource Initialize view binding

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //this code to setup the MainActivity to use view binding
+
+    lateinit var binding: ActivityMainBinding
+    //lateinit var promise that your code will initialize the variable before using it
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
